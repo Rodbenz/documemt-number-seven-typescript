83427a24a897ad316385d6dc6c39e96f471ae68b
Rodbenz <84115603+Rodbenz@users.noreply.github.com>
2023-06-22 15:24:48 +0700

    Update dataControl.tsx

--- libs/dataControl.tsx
+++ libs/dataControl.tsx
@@ -1,56 +1,56 @@
-// const Jwt = require('jsonwebtoken')
-// const convert = require("xml-js")
+const Jwt = require('jsonwebtoken')
+const convert = require("xml-js")
 
-// export function setCookie(cname:string, cvalue:string, exdays:number) {
-//     var d = new Date();
-//     let min = 60 * 24 // 5 minutes
-//     d.setTime(d.getTime() + (min * 60 * 1000)); // 5 minutes
-//     var expires = "expires=" + d.toUTCString();
-//     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-// }
+export function setCookie(cname:string, cvalue:string, exdays:number) {
+    var d = new Date();
+    let min = 60 * 24 // 5 minutes
+    d.setTime(d.getTime() + (min * 60 * 1000)); // 5 minutes
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
 
-// export function getCookie(cname:string) {
-//     if (process) {
-//         var name = cname + "=";
-//         console.log(name);
-//         var decodedCookie = decodeURIComponent(document.cookie);
-//         var ca = decodedCookie.split(';');
-//         for (var i = 0; i < ca.length; i++) {
-//             var c = ca[i];
-//             while (c.charAt(0) == ' ') {
-//                 c = c.substring(1);
-//             }
-//             if (c.indexOf(name) == 0) {
-//                 return c.substring(name.length, c.length);
-//             }
-//         }
-//         return "";
-//     }
-// }
+export function getCookie(cname:string) {
+    if (process) {
+        var name = cname + "=";
+        console.log(name);
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
+}
 
-// export function _createToken(data: any) {
-//     // try {
-//         var token = Jwt.sign(data, 'imyourfather!!!');
-//         return token
-//     // } catch {
-//     //     return false
-//     // }
-// }
+export function _createToken(data: any) {
+    // try {
+        var token = Jwt.sign(data, 'imyourfather!!!');
+        return token
+    // } catch {
+    //     return false
+    // }
+}
 
-// export function _readToken(token: any) {
-//     if (token == null) {
-//         return false
-//     } else {
-//         try {
-//         console.log(token, process.env.privateKey)
-//             var decode = Jwt.verify(token, 'imyourfather!!!');
-//             return decode
-//         } catch (err) {
-//             console.log(err)
-//             return falseF
-//         }
-//     }
-// }
+export function _readToken(token: any) {
+    if (token == null) {
+        return false
+    } else {
+        try {
+        console.log(token, process.env.privateKey)
+            var decode = Jwt.verify(token, 'imyourfather!!!');
+            return decode
+        } catch (err) {
+            console.log(err)
+            return false
+        }
+    }
+}
 
 export function right(str: any, chr: any) {
     try {
